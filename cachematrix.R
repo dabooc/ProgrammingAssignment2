a05d06e726e6e64220f220b94dd24ac2e5874aa2
dabooc <le.berred@free.fr>
2014-06-15 18:55:22 +0200

    First trial.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Allow to cache the invertion of a matrix
 
-## Write a short comment describing this function
+## Create a specific matrix object with a fied containing the inverted matrix if already computed
 
 makeCacheMatrix <- function(x = matrix()) {
+  cachedInvertion = null
 
+  ## retrieve the original matrix
+  get <- function() {
+    x
+  }
+  
+  ## cache the inverted matrix
+  setCachedInvertion(inv) {
+    cachedInvertion <<- inv
+  }
+  
+  ## retrieve the cached value
+  getCachedInvertion() {
+    cachedInvertion
+  }
+  
+  list(get = get,
+       setCachedInvertion = setCachedInvertion,
+       getCachedInvertion = getCachedInvertion)
 }
 
 
-## Write a short comment describing this function
+## Return the invert of a matrix. Takes advantage of caching to improve the process
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## check if the invert has already been computed
+        cached <- x$getCachedInvertion()
+        if (is.null(cached)) {
+            ## no, we need to compute it
+            cached <- solve(x$get())
+            ## and we cache it in the object
+            x$setCachedInvertion(cached)
+        }
+        cached
 }
